2147c8fcbe4ee0a0fced155613e5e31a87266a6e
asakasa <asakasa@me.com>
2015-07-31 02:33:40 +0900

    Add some template codes about VOTE and TALK into VILLAGER agent

--- src/jp/ac/kansai_u/kutc/firefly/aiwolf/agent/KobaVillager.java
+++ src/jp/ac/kansai_u/kutc/firefly/aiwolf/agent/KobaVillager.java
@@ -1,7 +1,13 @@
 package jp.ac.kansai_u.kutc.firefly.aiwolf.agent;
 
 import org.aiwolf.client.base.player.AbstractVillager;
+import org.aiwolf.client.lib.Utterance;
 import org.aiwolf.common.data.Agent;
+import org.aiwolf.common.data.Talk;
+import org.aiwolf.common.data.Vote;
+import org.aiwolf.common.net.GameInfo;
+
+import java.util.List;
 
 /**
  * This class is the role of Villager.
@@ -10,8 +16,49 @@ import org.aiwolf.common.data.Agent;
  * @author asakasa
  */
 public class KobaVillager extends AbstractVillager {
+    // Index of talks that already has been read in this day
+    int readTalkIdx = 0;
+
     @Override
     public void dayStart() {
+        readTalkIdx = 0;
+        for (Vote vote : getLatestDayGameInfo().getVoteList()) {
+        }
+    }
+
+    @Override
+    public void update(GameInfo gameInfo) {
+        super.update(gameInfo);
+
+        List<Talk> talkList = gameInfo.getTalkList();
+        for (int i = readTalkIdx; i < talkList.size(); i++) {
+            Utterance u = new Utterance(talkList.get(i).getContent());
+            switch (u.getTopic()) {
+                case COMINGOUT:
+                    break;
+                case VOTE:
+                    break;
+                case DIVINED:
+                    break;
+                case INQUESTED:
+                    break;
+                case GUARDED:
+                    break;
+                case ATTACK:
+                    break;
+                case ESTIMATE:
+                    break;
+                case AGREE:
+                    break;
+                case DISAGREE:
+                    break;
+                case SKIP:
+                    break;
+                case OVER:
+                    break;
+            }
+            readTalkIdx++;
+        }
 
     }
 
