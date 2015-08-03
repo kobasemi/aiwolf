0ef7a5c52c5fa24f7a624ae691057eeb9db96384
Kensuke Kosaka <k3n@trileg.net>
2015-08-03 20:03:11 +0900

    Add getPoint method

--- src/jp/ac/kansai_u/kutc/firefly/aiwolf/PointSystem.java
+++ src/jp/ac/kansai_u/kutc/firefly/aiwolf/PointSystem.java
@@ -11,10 +11,11 @@ import org.aiwolf.common.data.Team;
  * @author Kensuke Kosaka
  */
 public class PointSystem {
+	private AgentPoint mAgentPoint;
 
 	public PointSystem() {
-		// Generate AgentPoint instances of each player without me.
-		AgentPoint agentPoint = new AgentPoint();
+		// Generate mAgentPoint instances of each player without me.
+		mAgentPoint = new AgentPoint();
 	}
 
 
@@ -24,27 +25,60 @@ public class PointSystem {
 			if (point.getRole() != null) {
 				switch (point.getRole()) {
 					case Role.VILLAGER:
-						agentPoint.setVillagerPoint(point.getAgentId(), point.getView(), point.getVariation());
+						mAgentPoint.setVillagerPoint(point.getAgentId(), point.getView(), point.getVariation());
 						break;
 					case Role.BODYGUARD:
-						agentPoint.setBodyguardPoint(point.getAgentId(), point.getView(), point.getVariation());
+						mAgentPoint.setBodyguardPoint(point.getAgentId(), point.getView(), point.getVariation());
 						break;
 					case Role.SEER:
-						agentPoint.setSeerPoint(point.getAgentId(), point.getView(), point.getVariation());
+						mAgentPoint.setSeerPoint(point.getAgentId(), point.getView(), point.getVariation());
 						break;
 					case Role.MEDIUM:
-						agentPoint.setMediumPoint(point.getAgentId(), point.getView(), point.getVariation());
+						mAgentPoint.setMediumPoint(point.getAgentId(), point.getView(), point.getVariation());
 						break;
 					case Role.WEREWOLF:
-						agentPoint.setWolfPoint(point.getAgentId(), point.getView(), point.getVariation());
+						mAgentPoint.setWolfPoint(point.getAgentId(), point.getView(), point.getVariation());
 						break;
 					case Role.POSSESSED:
-						agentPoint.setPossessedPoint(point.getAgentId(), point.getView(), point.getVariation());
+						mAgentPoint.setPossessedPoint(point.getAgentId(), point.getView(), point.getVariation());
 						break;
 				}
 			} else {
-				agentPoint.setCampPoint(point.getAgentId(), point.getView(), point.getVariation());
+				mAgentPoint.setCampPoint(point.getAgentId(), point.getView(), point.getVariation());
 			}
 		}
 	}
-}
+
+
+
+	public int getPoint(Role role, int player, int view) {
+		int point = -1;
+		if (role != null) {
+			// Return IP.
+			switch (role) {
+				case Role.VILLAGER:
+					point = mAgentPoint.getVillagerPoint(player, view);
+					break;
+				case Role.BODYGUARD:
+					point = mAgentPoint.getBodyguardPoint(player, view);
+					break;
+				case Role.SEER:
+					point = mAgentPoint.getSeerPoint(player, view);
+					break;
+				case Role.MEDIUM:
+					point = mAgentPoint.getMediumPoint(player, view);
+					break;
+				case Role.WEREWOLF:
+					point = mAgentPoint.getWolfPoint(player, view);
+					break;
+				case Role.POSSESSED:
+					point = mAgentPoint.getPossessedPoint(player, view);
+					break;
+			}
+		} else {
+			// Return CP.
+			point = mAgentPoint.getCampPoint(player, view);
+		}
+
+		return point;
+	}
