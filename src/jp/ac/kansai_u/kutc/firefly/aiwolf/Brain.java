f74f3d707618518cd632d0aa7c1fc89379a5c13b
Bando <ppoponda@gmail.com>
2015-08-08 20:39:48 +0900

    updateメソッドの変数pointsの型をArrayList<Point>に変更 コメントのミスを修正

--- src/jp/ac/kansai_u/kutc/firefly/aiwolf/Brain.java
+++ src/jp/ac/kansai_u/kutc/firefly/aiwolf/Brain.java
@@ -1,10 +1,9 @@
 package jp.ac.kansai_u.kutc.firefly.aiwolf;
 
+import java.util.ArrayList;
+
 import org.aiwolf.common.data.Role;
 import org.aiwolf.common.data.Team;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * This class is 脳.
@@ -34,16 +33,16 @@ public class Brain {
 	// ポイント計算メソッド
 	public void update(Event event){
 
-		// PointSystemに渡すPoint<List>を作成
-		List<Point> points = new ArrayList<Point>();
+		// PointSystemに渡すArrayList<Point>を作成
+		ArrayList<Point> points = new ArrayList<Point>();
 
 		/*
 		 TODO; EventやAgentPoint等から増減するポイントを計算
 		 TODO; Pointインスタンスを生成
-		 TODO; Point<List>に追加
+		 TODO; ArrayList<Point>に追加
 		 */
 
-		// PointSystemにPoint<List>を渡す
+		// PointSystemにArrayList<Point>を渡す
 		pointSystem.updatePoint(points);
 	}
 }
