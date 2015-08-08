3ed78bb13fbf1ca1bdb033e492a78c9e5a8def63
Kensuke Kosaka <k3n@trileg.net>
2015-08-08 21:16:11 +0900

    初日の役職者COの内訳別に処理を分岐させる記述を追加

--- src/jp/ac/kansai_u/kutc/firefly/aiwolf/Brain.java
+++ src/jp/ac/kansai_u/kutc/firefly/aiwolf/Brain.java
@@ -3,6 +3,7 @@ package jp.ac.kansai_u.kutc.firefly.aiwolf;
 import org.aiwolf.common.data.Role;
 import org.aiwolf.common.data.Team;
 
+import java.lang.Integer;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,6 +18,9 @@ public class Brain {
 	private Role myRole;
 	private Team myTeam;
 	private PointSystem pointSystem;
+
+	private ArrayList<Integer> firstDayCoSeer = new ArrayList<Integer>();
+	private ArrayList<Integer> firstDayCoMedium = new ArrayList<Integer>();
 
 	// コンストラクタ
 	public Brain(){
@@ -42,6 +46,40 @@ public class Brain {
 		 TODO; Pointインスタンスを生成
 		 TODO; Point<List>に追加
 		 */
+		if (event.getDay() == 0) {
+			if (event.getType() == "COMINGOUT") {
+				// 初日の占い師および霊能師のCOをカウントする
+				if (event.getRole() == "Seer") {
+					if (!firstDayCoSeer.contains(event.getAgent().getAgentIdx()))
+						firstDayCoSeer.add(event.getAgent().getAgentIdx());
+				} else if (event.getRole() == "Medium") {
+					if (!firstDayCoMedium.contains(event.getAgent().getAgentIdx()))
+						firstDayCoMedium.add(event.getAgent().getAgentIdx());
+				}
+			}
+		} else {
+			if (firstDayCoSeer.size() == 0 && firstDayCoMedium.size() == 0) {
+				// 占い師および霊能師のCOがともに0人だった場合
+				// 役職者を暴こうとする人，発言数が少ない人にマイナスを振る
+
+			} else if (firstDayCoSeer.size() == 1 && (firstDayCoMedium.size() == 1 || firstDayCoMedium.size() == 2)) {
+				// 占い師1，霊能師1あるいは2の場合
+				// 正しい占い師に関して，占い結果が出る2日目まで潜伏する可能性があり，このときのCOが正しいとは限らないが，基本的には信用する
+
+			} else if (firstDayCoSeer.size() == 2 && (firstDayCoMedium.size() == 1 || firstDayCoMedium.size() == 2)) {
+				// 占い師2，霊能師1あるいは2の場合
+				// 占い師に間違いなく騙りがいる．内訳は占い師・狼・狂人．これら役職のポイントをいじる
+
+			} else if (firstDayCoSeer.size() == 3 && firstDayCoMedium.size() == 1) {
+				// 占い師3，霊能師1の場合
+				// 占い師に間違いなく騙りがいる．占い候補者が噛まれたら他の占い師に関してポイントを下げていく．
+
+			} else if (firstDayCoSeer.size() == 4 && firstDayCoMedium.size() == 1) {
+				// 占い師4，霊能師1の場合
+				// 占いCOした人に対して，ポイントを下げていく
+
+			}
+		}
 
 		// PointSystemにPoint<List>を渡す
 		pointSystem.updatePoint(points);
