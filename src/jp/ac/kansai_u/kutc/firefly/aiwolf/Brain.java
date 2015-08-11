473128ce1522dc9b00b516c4b11b5f3919f6724c
Kensuke Kosaka <k3n@trileg.net>
2015-08-11 18:21:54 +0900

    占い師4，霊能師1の場合において，占い師COした者のポイントを操作する処理を追加

--- src/jp/ac/kansai_u/kutc/firefly/aiwolf/Brain.java
+++ src/jp/ac/kansai_u/kutc/firefly/aiwolf/Brain.java
@@ -20,17 +20,21 @@ public class Brain {
 	private ArrayList<Integer> firstDayCoSeer = new ArrayList<Integer>();
 	private ArrayList<Integer> firstDayCoMedium = new ArrayList<Integer>();
 
+	private boolean updatePointByCoFlag;
+
 	// コンストラクタ
 	public Brain(){
 
 		// PointSystemのインスタンス化
 		this.pointSystem = new PointSystem();
+		updatePointByCoFlag = false;
 	}
 
 	public Brain(Role role, Team team) {
 		this.myRole = role;
 		this.myTeam = team;
 		this.pointSystem = new PointSystem();
+		updatePointByCoFlag = false;
 	}
 
 	// ポイント計算メソッド
@@ -46,7 +50,8 @@ public class Brain {
 		 */
 		if (event.getDay() == 0) {
 			if (event.getType() == "COMINGOUT") {
-				// 初日の占い師および霊能師のCOをカウントする
+				//region 初日の自分以外の占い師および霊能師のCOをカウントする
+				//TODO もし自分自身の発言もeventにカウントされるなら，それを取り除く
 				if (event.getRole().equals(Role.SEER)) {
 					if (!firstDayCoSeer.contains(event.getAgent().getAgentIdx()))
 						firstDayCoSeer.add(event.getAgent().getAgentIdx());
@@ -54,6 +59,7 @@ public class Brain {
 					if (!firstDayCoMedium.contains(event.getAgent().getAgentIdx()))
 						firstDayCoMedium.add(event.getAgent().getAgentIdx());
 				}
+				//endregion
 			}
 		} else {
 			if (firstDayCoSeer.size() == 0 && firstDayCoMedium.size() == 0) {
@@ -74,12 +80,93 @@ public class Brain {
 
 			} else if (firstDayCoSeer.size() == 4 && firstDayCoMedium.size() == 1) {
 				// 占い師4，霊能師1の場合
-				// 占いCOした人に対して，ポイントを下げていく
 
+				//region 2日目においてこのメソッドが初めて呼び出された時に，占いCOした人に対してポイントを下げる
+				if (event.getDay() == 1 && updatePointByCoFlag == false) {
+
+					//region 客観ポイント
+					for (int i = 0; i < firstDayCoSeer.size(); i++) {
+						// 占い師のポイントを下げる
+						Point decreaseSeer = new Point();
+						decreaseSeer.setVariation(-10);
+						decreaseSeer.setAgentId(firstDayCoSeer.get(i));
+						decreaseSeer.setView(1);
+						decreaseSeer.setRole(Role.SEER);
+						points.add(decreaseSeer);
+
+						// 人狼のポイントを上げる
+						Point increaseWerewolf = new Point();
+						increaseWerewolf.setVariation(10);
+						increaseWerewolf.setAgentId(firstDayCoSeer.get(i));
+						increaseWerewolf.setView(1);
+						increaseWerewolf.setRole(Role.WEREWOLF);
+						points.add(increaseWerewolf);
+
+						// 狂人のポイントを上げる
+						Point increasePossessed = new Point();
+						increasePossessed.setVariation(10);
+						increasePossessed.setAgentId(firstDayCoSeer.get(i));
+						increasePossessed.setView(1);
+						increasePossessed.setRole(Role.POSSESSED);
+						points.add(increasePossessed);
+
+						// 陣営ポイントを下げる
+						Point decreaseCP = new Point();
+						decreaseCP.setVariation(-10);
+						decreaseCP.setAgentId(firstDayCoSeer.get(i));
+						decreaseCP.setView(1);
+						points.add(decreaseCP);
+					}
+					//endregion
+
+					//region 主観ポイント
+					if (myRole == Role.SEER) {
+						// 自分以外で占い師COしたものをすべて人狼または狂人とみなす
+						for (int i = 0; i < firstDayCoSeer.size(); i++) {
+							// 占い師のポイントを最小にする
+							Point decreaseSeer = new Point();
+							decreaseSeer.setVariation(-100);
+							decreaseSeer.setAgentId(firstDayCoSeer.get(i));
+							decreaseSeer.setView(0);
+							decreaseSeer.setRole(Role.SEER);
+							points.add(decreaseSeer);
+
+							// 人狼のポイントを最大にする
+							Point increaseWerewolf = new Point();
+							increaseWerewolf.setVariation(100);
+							increaseWerewolf.setAgentId(firstDayCoSeer.get(i));
+							increaseWerewolf.setView(0);
+							increaseWerewolf.setRole(Role.WEREWOLF);
+							points.add(increaseWerewolf);
+
+							// 狂人のポイントを最大にする
+							Point increasePossessed = new Point();
+							increasePossessed.setVariation(100);
+							increasePossessed.setAgentId(firstDayCoSeer.get(i));
+							increasePossessed.setView(0);
+							increasePossessed.setRole(Role.POSSESSED);
+							points.add(increasePossessed);
+
+							// 陣営ポイントを最小にする
+							Point decreaseCP = new Point();
+							decreaseCP.setVariation(-100);
+							decreaseCP.setAgentId(firstDayCoSeer.get(i));
+							decreaseCP.setView(0);
+							points.add(decreaseCP);
+						}
+					}
+					//endregion
+
+					updatePointByCoFlag = true;
+				}
+				//endregion
 			}
 		}
 
 		// PointSystemにArrayList<Point>を渡す
 		pointSystem.updatePoint(points);
+
+		// pointsの中身を空にする
+		points.clear();
 	}
 }
